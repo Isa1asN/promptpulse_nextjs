c8a399b1b1fe84c79d6ceffc9d4da562e3cbc3f1
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-08 13:18:38 -0700

    added some files

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,14 @@
 function Home() {
   return (
     <section className='w-full flex-center flex-col'>
-        <h1 className="head_text">
+        <h1 className="head_text text-center">
             Discover and Share 
-
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI Powered Prompts</span>
         </h1>
+
+        <p className="desc text-center">PromptPulse is a tool for the modern world to discover, create and share creative prompts</p>
+
     </section>
   )
 }
