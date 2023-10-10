82c56623ff9d570a249f45a1b05fd5e3b844bbc3
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-10 11:25:38 -0700

    added changes, issues still there

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,31 +10,33 @@ const handler = NextAuth({
             clientSecret : process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({session}){
-        const sessionUser = User.findOne({email : session.user.email})
-        session.user.id = sessionUser._id
-        return session
-    },
-    async signIn({profile}) {
-        try {
-            await connectDB()
-
-            const userExist = await User.findOne({email : profile.email})
-            if(!userExist){
-                await User.create({
-                    email : profile.email,
-                    username : profile.name.replace(' ', '').toLowerCase(),
-                    image : profile.image,
-                })
+    callbacks : {
+        async session({session}){
+            const sessionUser = User.findOne({email : session.user.email})
+            session.user.id = sessionUser._id
+            return session
+        },
+        async signIn({profile}) {
+            try {
+                await connectDB()
+    
+                const userExist = await User.findOne({email : profile.email})
+                if(!userExist){
+                    await User.create({
+                        email : profile.email,
+                        username : profile.name.replace(' ', '').toLowerCase(),
+                        image : profile.image,
+                    })
+                }
+    
+                return true
+                
+            } catch (error) {
+                console.log(error)
+                return false
             }
+        },
 
-            return true
-            
-        } catch (error) {
-            console.log(error)
-            return false
-        }
-    }
-})
+    },})
 
 export {handler as GET, handler as POST}
